3576c4df92bee4880f0dcb45fb6633310e88fa4b
Peter Hirschfeld <burnde@gmx.de>
2012-11-13 16:44:47 +0100

    add indexing

--- src/Crawler.java
+++ src/Crawler.java
@@ -1,9 +1,12 @@
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -11,10 +14,29 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.store.IOContext;
+import org.apache.lucene.store.IndexInput;
+import org.apache.lucene.store.IndexOutput;
+import org.apache.lucene.util.Version;
+
 
 public class Crawler {
 	private LinkedList<String> URLList= new LinkedList<String>();
 	private HashMap<String, Date> VisitedUrls = new HashMap<String, Date>();
+	
+	Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_40);
+	IndexWriterConfig iwc = new IndexWriterConfig(Version.LUCENE_40, analyzer);
 	
 	public void addURL(String url)
 	{
@@ -46,6 +68,43 @@ public class Crawler {
 				VisitedUrls.put(url, new Date()); // record that the url was visited, at the current time
 				
 				// TODO do something with content ...
+				Document doc = new Document();
+				
+				try {
+					iwc.setOpenMode(OpenMode.CREATE);
+					
+					String indexPath = "/home/burnde/index/";
+					Directory dir = FSDirectory.open(new File(indexPath));
+					
+					System.out.println("Indexing to directory '" + indexPath + "'...");
+					IndexWriter writer = new IndexWriter(dir,iwc);
+					
+					Field url_field = new StringField("URL", url, Field.Store.YES);
+					doc.add(url_field);
+					
+					//Field url_title = new StringField("Title", url, Field.Store.YES);
+					//doc.add(url_field);
+					
+					//Field url_content = new StringField("Content", content, Field.Store.YES);
+					doc.add(new TextField("Contents", new StringReader(content)));
+					//doc.add(url_content);
+					
+					if (writer.getConfig().getOpenMode() == OpenMode.CREATE) {
+						// New index, so we just add the document (no old document can be there):
+						System.out.println("adding " + url);
+						writer.addDocument(doc);
+					} else {
+						// Existing index (an old copy of this document may have been indexed) so 
+						// we use updateDocument instead to replace the old one matching the exact 
+						// path, if present:
+						System.out.println("updating " + url);
+						//writer.updateDocument(new Term("path", file.getPath()), doc);
+					}
+					
+					writer.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 				
 				List<String> links = extractURLs(content);
 				
